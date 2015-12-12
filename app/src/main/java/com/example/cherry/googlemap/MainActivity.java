96e82c0d6e07c52214b6d802c6e7f66ad142c803
KangJiHyung <wlgud614@google.com>
2015-12-12 16:43:38 +0900

    GoogleMap + GPS

---
+++ app/src/main/java/com/example/cherry/googlemap/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.cherry.googlemap;
+
+import android.app.Activity;
+import android.content.Context;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import android.location.Location;
+import android.util.Log;
+import android.widget.TextView;
+
+
+public class MainActivity extends Activity {
+
+    TextView logView;
+
+    //static final LatLng SEOUL = new LatLng( 37.56, 126.97);
+    private GoogleMap map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //logView = (TextView) findViewById(R.id.log);
+
+        // Acquire a reference to the system Location Manager
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        // GPS 프로바이더 사용가능여부
+        boolean isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        // 네트워크 프로바이더 사용가능여부
+        boolean isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+        Log.d("Main", "isGPSEnabled=" + isGPSEnabled);
+        Log.d("Main", "isNetworkEnabled=" + isNetworkEnabled);
+
+        LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                double lat = location.getLatitude();
+                double lng = location.getLongitude();
+
+                final LatLng MyLocation = new LatLng(lat, lng);
+
+                map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+                map.addMarker(new MarkerOptions().position(MyLocation).title("My Location"));
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(MyLocation, 15));
+
+                //map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+                logView.setText("onStatusChanged");
+            }
+
+            public void onProviderEnabled(String provider) {
+                logView.setText("onProviderEnabled");
+            }
+
+            public void onProviderDisabled(String provider) {
+                logView.setText("onProviderDisabled");
+            }
+        };
+
+        // Register the listener with the Location Manager to receive location updates
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+    }
+
+}
